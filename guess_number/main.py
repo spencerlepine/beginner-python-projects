203fc17de71bd66642bfac097cd1a27f7db90427
Spencer Lepine <spencerlepine26@gmail.com>
2021-01-11 08:24:33 -0500

    Add madlibs and number guesser

---
+++ guess_number/main.py
@@ -0,0 +1,40 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+
+    # Guess can only start greater than 1
+    guess = 0
+
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        print(guess)
+
+        if guess < random_number:
+            print("Sorry guess again, too low..")
+        elif guess > random_number:
+            print("Sorry guess again, too high..")
+
+    print(f"Congrats, you have guessed {random_number}!")
+
+#guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+
+    while feedback != "c":
+        # The bounds will be updated each guess
+        guess = random.randint(low, high)
+
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C): ").lower()
+
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+
+    print(f"The computer found your number: {x}")
+
+computer_guess(int(input("Pick a number greater than 0: ")))
---
+++ madlib/madlibs.py
@@ -0,0 +1,16 @@
+#@KylieYing (12 beginner python projects - coding course)
+
+# youtuber = "Spencer Lepine"
+
+# # String concatenation
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time becuase \
+I love to {verb1}. Stay hyrdrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
